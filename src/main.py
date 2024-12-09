4ff98e1d9aa973d52cda039b61c6a58a83ed2fb7
Migrok <90355004+Migrok@users.noreply.github.com>
2024-12-09 21:33:21 +0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import telebot
+from decouple import config
+
+bot = telebot.TeleBot(config('TOKEN'))
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    with open('image.jpg', 'rb') as photo:
+        bot.send_photo(message.chat.id, photo)
+
+
+if __name__ == '__main__':
+    try:
+        print("Бот запущен")
+        bot.infinity_polling()
+    except Exception as e:
+        print(f"Ошибка: {e}")
