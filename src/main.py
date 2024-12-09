a7efc0ff020cc53c31294e4cdda47d74396d413f
Migrok <90355004+Migrok@users.noreply.github.com>
2024-12-09 23:51:19 +0300

    test parse 2

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import telebot
 from decouple import config
+from parser import parse_steam
 
 bot = telebot.TeleBot(config('TOKEN'))
 
@@ -8,6 +9,11 @@ def send_welcome(message):
     with open('image.jpg', 'rb') as photo:
         bot.send_photo(message.chat.id, photo)
 
+@bot.message_handler(commands=['parse'])
+def get_prices(message):
+    bot.reply_to(message, "Паршу")
+    price_data = parse_steam()
+    bot.reply_to(message, f"{price_data}")
 
 
 if __name__ == '__main__':
--- src/parser.py
+++ src/parser.py
@@ -6,24 +6,31 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-def get_price():
-    driver_path = "../chromedriver/chromedriver.exe"
+def parse_steam():
+    print("start parsing")
+    driver_path = "/usr/bin/chromedriver"
     driver = webdriver.Chrome(service=Service(driver_path))
-
-    url = "https://steamcommunity.com/market/listings/730/Charm%20%7C%20Die-cast%20AK"
+    print("driver on")
+    url = "https://steamcommunity.com/market/listings/730/Charm%20%7C%20Die-cast%20AK/render/?query=&start=0&count=100&country=RU&language=russian&currency=5"
     driver.get(url)
+    price_data = ''
 
     try:
+        print("trying to connect")
         WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.CLASS_NAME, "market_listing_price"))
         )
 
-        item_names = driver.find_elements(By.CLASS_NAME, "market_listing_row_details_attribute")[:10]
-        prices = driver.find_elements(By.CLASS_NAME, "market_listing_price_with_fee")[:10]
+        item_names = driver.find_elements(By.CLASS_NAME, "market_listing_row_details_attribute")[:20]
+        prices = driver.find_elements(By.CLASS_NAME, "market_listing_price_with_fee")[:20]
 
         time.sleep(2)
 
+
         for i in range(min(10, len(item_names))):
+            price_data += f"{item_names[i].text} = {prices[i].text}\n"
             print(f"{item_names[i].text} = {prices[i].text}")
     finally:
+        print("closing driver")
         driver.quit()
+    return price_data
